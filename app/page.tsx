19a88c894e61e2371d9a3681f4f6c6288c942648
Ibrahim Ibrahim <eebrahiim04@gmail.com>
2026-01-16 19:40:22 +0100

    feat: add home page and event card components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import events from "@/lib/constants";
+
 const Page = () => {
   return (
     <section>
       <h1 className="text-center">
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         The Hub for Every Dev <br /> Event You Can't Miss
       </h1>
       <p className="text-center mt-5">
         Hackatons, Meetups, and Conferences, All in One Place
       </p>
+
+      <ExploreBtn />
+
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
